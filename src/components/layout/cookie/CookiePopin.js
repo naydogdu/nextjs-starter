34b3c883258fbe4880701a58ac10a8b18ab6d458
Aydogdu Nazmi <nazmi@grapheek.com>
2022-05-27 14:25:36 +0200

    minor change

--- src/components/layout/cookie/CookiePopin.js
+++ src/components/layout/cookie/CookiePopin.js
@@ -5,7 +5,7 @@ import {Button} from "../../ui/Button"
 
 const CookiePopin = ({dispatch}) => (
     <aside className={[
-        "fixed z-50 bottom-0 text-center right-0 bg-primary text-white py-4 px-8",
+        "fixed z-50 bottom-0 text-center lg:text-left right-0 bg-primary text-white py-4 px-6",
         "flex flex-col gap-4 lg:flex-row lg:items-center lg:max-w-screen-lg",
     ].join(' ')}>
         <p className={"text-sm leading-relaxed"}>
