6d5446c86d66bb1a59a97c4b032ae6a9d13fd9ba
Nazmi Aydogdu <nazmi@grapheek.com>
2023-06-08 13:59:54 +0200

    minor fix

--- src/components/ui/Image.js
+++ src/components/ui/Image.js
@@ -5,7 +5,7 @@ const Image = props => {
 
     return (
         <>
-            {props.roller && <Rolling className={"absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-24 h-24 text-primary"} />}
+            {props.roller && <Rolling className={"absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-24 h-24"} />}
             <NextImage
                 layout={"fill"}
                 objectFit={"cover"}
