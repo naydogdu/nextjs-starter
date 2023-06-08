a2bf827a78fe99b0b8c5b8d7f17b89dd51dfc5fe
Nazmi Aydogdu <nazmi@grapheek.com>
2023-06-08 13:57:52 +0200

    pursuit

--- src/components/ui/Image.js
+++ src/components/ui/Image.js
@@ -1,15 +1,21 @@
 import NextImage from "next/image"
+import Rolling from "../../images/rolling.svg"
 
 const Image = props => {
 
     return (
-        <NextImage
-            layout={"fill"}
-            objectFit={"cover"}
-            objectPosition={"center"}
-            alt={""}
-            {...props}
-        />
+        <>
+            {props.roller && <Rolling className={"absolute left-1/2 top-1/2 -translate-y-1/2 -translate-x-1/2 w-24 h-24 text-primary"} />}
+            <NextImage
+                layout={"fill"}
+                objectFit={"cover"}
+                objectPosition={"center"}
+                alt={""}
+                quality={90}
+                {...props}
+                roller={null}
+            />
+        </>
     )
 }
 
