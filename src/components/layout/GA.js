5dd0f1261c8c6ea4f44dd3552a098ac72985a8f6
Nazmi Aydogdu <nazmi@grapheek.com>
2023-06-21 17:02:45 +0200

    GA update

--- src/components/layout/GA.js
+++ src/components/layout/GA.js
@@ -13,27 +13,31 @@ const GA = () => {
                 <Script id={"g-tag"} src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
             }
             {(text.app.activeGa && ((cookieConsentState.isSet > 0 && cookieConsentState.marketing > 0) || !cookieConsentState.isSet)) &&
-                <Script id={"g-tag-tracker"}>
-                    {`
+                <Script
+                    id={"g-tag-tracker"}
+                    strategy={"beforeInteractive"}
+                    dangerouslySetInnerHTML={{ __html: `
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){dataLayer.push(arguments);}
                         gtag('js', new Date());
                         gtag('config', '${GA_TRACKING_ID}', {
                           page_path: window.location.pathname,
                         });
-                    `}
-                </Script>
+                    `}}
+                />
             }
             {(text.app?.activeGtm && ((cookieConsentState.isSet > 0 && cookieConsentState.marketing > 0) || !cookieConsentState.isSet)) &&
-                <Script id={"gtm-tracker"}>
-                    {`
+                <Script
+                    id={"gtm-tracker"}
+                    strategy={"beforeInteractive"}
+                    dangerouslySetInnerHTML={{ __html: `
                         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                         })(window,document,'script','dataLayer','${GTM_TRACKING_ID}');
-                    `}
-                </Script>
+                    `}}
+                />
             }
         </>
     )
