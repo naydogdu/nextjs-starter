dfec891ec820972b0cb3a98090d5561b5950deb9
Nazmi Aydogdu <nazmi@grapheek.com>
2023-06-21 15:35:39 +0200

    update GA

--- src/components/layout/GA.js
+++ src/components/layout/GA.js
@@ -1,44 +1,43 @@
 import text from "../../../content/main.json"
+
 import Script from "next/script"
 import {GA_TRACKING_ID, GTM_TRACKING_ID} from "../../utils/gtag"
 import {useCookieConsentState} from "./cookie/CookieConsent"
+import {useEffect} from "react"
 
 const GA = () => {
     const cookieConsentState = useCookieConsentState()
+
+    useEffect(() => {
+    }, [cookieConsentState])
 
     return (
         <>
             {text.app.activeGa &&
                 <Script id={"g-tag"} src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
             }
-            {(text.app.activeGa && (cookieConsentState.isSet > 1 || cookieConsentState.marketing > 0)) &&
-                <Script
-                    id={"g-tag-tracker"}
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                            gtag('config', '${GA_TRACKING_ID}', {
-                              page_path: window.location.pathname,
-                            });
-                        `,
-                    }}
-                />
+            {(text.app.activeGa && ((cookieConsentState.isSet > 0 && cookieConsentState.marketing > 0) || !cookieConsentState.isSet)) &&
+                <Script id={"g-tag-tracker"} strategy={"beforeInteractive"}>
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${GA_TRACKING_ID}', {
+                          page_path: window.location.pathname,
+                        });
+                    `}
+                </Script>
             }
-            {(text.app?.activeGtm && (cookieConsentState.isSet > 1 || cookieConsentState.marketing > 0)) &&
-                <Script
-                    id={"gtm-tracker"}
-                    dangerouslySetInnerHTML={{
-                        __html: `
-                            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                            })(window,document,'script','dataLayer','${GTM_TRACKING_ID}');
-                        `,
-                    }}
-                />
+            {(text.app?.activeGtm && ((cookieConsentState.isSet > 0 && cookieConsentState.marketing > 0) || !cookieConsentState.isSet)) &&
+                <Script id={"gtm-tracker"} strategy={"beforeInteractive"}>
+                    {`
+                        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                        })(window,document,'script','dataLayer','${GTM_TRACKING_ID}');
+                    `}
+                </Script>
             }
         </>
     )
